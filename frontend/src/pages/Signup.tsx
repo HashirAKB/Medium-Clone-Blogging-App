6a7c258f8367977b28ff1274c169d162faa60f2c
HashirAKB <ahmedhashir96@gmail.com>
2024-06-12 13:42:48 +0530

    Signup component [WIP]

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,10 +1,11 @@
-import { Feedback } from "../components/feedback"
+import { Auth } from "../components/Auth"
+import { Feedback } from "../components/Feedback"
 
 export const SignUp = () => {
     return (
         <div className="grid grid-cols-2">
             <div>
-
+                <Auth/>
             </div>
             <div className='invisible lg:visible'>
                 <Feedback/>
