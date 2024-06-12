4a1bd379f64c193f94cbb57b40a809c5ddd585bb
HashirAKB <ahmedhashir96@gmail.com>
2024-06-12 13:10:09 +0530

    Completed feedback banner component.

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,7 +1,14 @@
+import { Feedback } from "../components/feedback"
+
 export const SignUp = () => {
     return (
-        <>
-            Signup Page
-        </>
+        <div className="grid grid-cols-2">
+            <div>
+
+            </div>
+            <div className='invisible lg:visible'>
+                <Feedback/>
+            </div>
+        </div>
     )
 }
